d8f6fad7a0aca9be650d29de1961bfd8564b91d6
Andreas Grip <andreas.grip@gmail.com>
2025-05-13 23:18:58 +0200

    First version

---
+++ example.js
@@ -0,0 +1,100 @@
+const Webserver = require('griffinwebserver_v2');
+const session = require('griffinwebserver_v2-session');
+const webserver = new Webserver('www/', 8080);
+webserver.start();
+
+function isJSON(str) {
+  try {
+    JSON.parse(str);
+    return true;
+  } catch (e) {
+    return false;
+  }
+}
+
+function httpFormToObject(formData) {
+  // if it's not a string it's not formdata
+  if (typeof formData !== 'string') return formData;
+  // if it's valid json it's not formdata
+  if (!isJSON(formData)) return formData;
+  // if there is no equals sign it's not formdata
+  if(formData.indexOf('=') === -1) return formData;
+
+  
+  const formDataObj = {};
+  const formDataArr = formData.split('&');
+  for (let i = 0; i < formDataArr.length; i++) {
+    const pair = formDataArr[i].split('=');
+    formDataObj[pair[0]] = decodeURIComponent(pair[1]);
+  }
+  return formDataObj;
+}
+
+
+function apiFunction(req, res) {
+  const baseUrl = 'http://' + req.headers.host + '/';
+  const parsedUrl = new URL(req.url, baseUrl);
+
+  // split the path into parts after removing leading /
+  const pathParts = parsedUrl.pathname.substring(1).split('/');
+  const apiName = pathParts[1]; // the second part of the path is the API name, first is /api/
+
+  let responseBody;
+  let body = []; // any posted data will go here
+  let data = {};
+
+  req.on('data', (data) => {
+    body.push(data)
+  });
+
+  req.on('end', function () {
+    dataDone();
+  })
+
+  function dataDone() {
+    // convert posted data to data
+    body = Buffer.concat(body).toString(); // convert to string
+    body = parseFormData(body); // parse the data in to an object if posted form
+    if(isJSON(body)) body = JSON.parse(body); // parse the data if it is json
+
+    req.session.data.counter = (req.session.data.counter || 0) + 1; // increment the counter of request made in this session
+    req.statusCode = 200; // default status is Ok
+    switch(apiName) {
+      case 'hello':
+        responseBody = 'Hello, world!';
+        break;
+      case 'counter':
+        responseBody = 'Counter: ' + req.session.data.counter;
+        break;
+      case 'login':
+        if(body.username === 'admin' && body.password === 'password') {
+          req.session.data.loggedIn = true;
+          
+          responseBody = 'Logged in';
+        } else {
+          req.session.data.loggedIn = false;
+          req.statusCode = 401;
+          responseBody = 'Invalid username or password';
+        }
+       break;
+      case 'logout':
+        if(req.session.data.loggedIn) {
+          req.session.data.loggedIn = false;
+          responseBody = 'Logged out';
+        } else {
+          req.statusCode = 401;
+          responseBody = 'Not logged in';
+        }
+       break;
+        default:
+          req.statusCode = 404;
+          responseBody = 'Invalid API name';
+    }
+
+    res.end(responseBody);
+  }
+}
+
+webserver.addMiddleware(session);
+
+webserver.addHandler('/api/', apiFunction);
---
+++ session.js
@@ -0,0 +1,45 @@
+const crypto = require('crypto');
+const cookie = require('cookie');
+
+const ttl = 1000 * 60 * 60 * 12; // 12 hours in milliseconds
+
+// sessions will be kept in both a array and object for convenience
+var sessions = {};
+const sessionsArray = [];
+
+setInterval(pruneSessions, 60 * 1000); // prune every minute
+
+function pruneSessions() {
+  for (let i = 0; i < sessions.length; i++) {
+    if (sessions[i].createdAt + ttl >= Date.now()) { // if session is expired, delete it
+      delete sessions[i]; // delete it from object
+      sessionsArray.splice(i, 1); // delete it from array
+      i--;
+    }
+  }
+}
+
+// helper function
+function generateSessionId() {
+  let sessionId = crypto.randomBytes(32).toString('hex');
+  if (sessions[sessionId]) sessionId = generateSessionId(); // generate a new id if the generated id is already in use
+  return sessionId;
+}
+
+function session(req, res) {
+  // check for session id in cookie
+  const cookies = req?.headers?.cookie ? cookie.parse(req.headers.cookie) : {};
+  let sessionId = cookies?.session_id;
+  // if cookie is present, check for session in sessions object
+  if (!sessionId || !sessions[sessionId]) {
+    sessionId = generateSessionId();
+    sessions[sessionId] = { id: sessionId, expires: Date.now() + ttl, createdAt: Date.now(), accessedAt: Date.now(), data: {} };
+    sessionsArray.push(sessions[sessionId]);
+    res.setHeader('Set-Cookie', `session_id=${sessionId}; path=/; max-age=${ttl}; httpOnly; sameSite=strict`);
+  } else {
+    // if session is found in sessions object, only update accessedAt
+    sessions[sessionId].accessedAt = Date.now();
+  }
+}
+
+exports.default = session;
